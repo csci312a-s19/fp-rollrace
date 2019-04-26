91daad29043731da7a296a5b2c55143546c67486
anthony-turcios <aturcios@middlebury.edu>
2019-04-26 03:18:31 +0000

    added mapchoices template with simple test

---
+++ client/src/MapChoice.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { Row, Col, Card, CardBody, CardTitle, CardImg } from 'reactstrap';
+export default function MapChoice(props) {
+  const { func } = props;
+  return (
+    <Row id="maps" class="row">
+      <a
+        style={{ cursor: 'pointer' }}
+        onClick={() => {
+          func('Map1');
+        }}
+      >
+        <Col xs={12} sm={6} md={3}>
+          <Card>
+            <CardBody>
+              <CardTitle>Map 1</CardTitle>
+              <CardImg top width="100%" src="https://placebeard.it/640x360" />
+            </CardBody>
+          </Card>
+        </Col>
+      </a>
+      <Col
+        xs={12}
+        sm={6}
+        md={3}
+        onClick={() => {
+          func('Map1');
+        }}
+      >
+        <Card>
+          <CardBody>
+            <CardTitle>Map 2</CardTitle>
+            <CardImg top width="100%" src="https://placebear.com/640/360" />
+          </CardBody>
+        </Card>
+      </Col>
+      <Col
+        xs={12}
+        sm={6}
+        md={3}
+        onClick={() => {
+          func('Map1');
+        }}
+      >
+        <Card>
+          <CardBody>
+            <CardTitle>Map 3</CardTitle>
+            <CardImg top width="100%" src="https://picsum.photos/640/360" />
+          </CardBody>
+        </Card>
+      </Col>
+    </Row>
+  );
+}
---
+++ client/src/MapChoice.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import MapChoice from './MapChoice.js';
+import { mount } from 'enzyme';
+
+describe('MapChoice test', () => {
+  test('Possible Maps should be visible', () => {
+    const comp = mount(<MapChoice />);
+    expect(comp.find('id="maps"')).toBeVisible();
+  });
+});
